2eff0548508c12b11292a9c47e8c80b7f2eb6195
williams-z <williams.z.bis@gmail.com>
2022-05-19 00:37:05 +1200

    nav sort of working but CSS fucks me over once again

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
+import Nav from './Nav'
+
 export default function Header() {
+  const [navOpen, setNavOpen] = useState(false)
+
+  function toggleNav() {
+    setNavOpen(!navOpen)
+  }
+
+
   return (
+    <>
     <div className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <div className="nav-logo">
             <p>SubWoofer</p>
           </div>
-          <button type='button' className='btn nav-btn'>
+          <button type='button' className='btn nav-btn' onClick={toggleNav}>
             <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
           </button>
         </div>
       </div>
     </div>
+      <nav className={`overlay${navOpen ? ' overlay--open' : ''}`} onClick={toggleNav}>
+          <Nav />
+      </nav>
+    </>
   )
 }
